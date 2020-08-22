304e965712c23b3a6148094b7c10a178626ed289
LiZhi6 <318165993@qq.com>
2020-08-22 14:25:08 +0800

    添加userService

---
+++ src/main/java/com/qf/service/impl/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package com.qf.service.impl;
+
+import com.qf.pojo.User;
+import com.qf.service.UserService;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public List<User> findAll() {
+        return null;
+    }
+}
